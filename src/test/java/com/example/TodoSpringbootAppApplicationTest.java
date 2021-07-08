4ad8e0b61a929c057f368e9da924dbba7cbb52bf
aleks <aleks94@me.com>
2021-07-09 00:33:26 +0200

    commit

--- src/test/java/com/example/TodoSpringbootAppApplicationTest.java
+++ src/test/java/com/example/TodoSpringbootAppApplicationTest.java
@@ -1,12 +1,16 @@
+package com.example;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class TodoSpringbootAppApplicationTests {
+@SpringBootApplication
+public class TodoSpringbootAppApplicationTest {
 
     @Test
     public void contextLoads() {
